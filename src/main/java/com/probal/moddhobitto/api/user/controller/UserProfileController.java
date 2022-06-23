99e58601ae4380c466075ece60cea9ab910fbf66
probal <probal25@hotmail.com>
2022-06-23 21:47:01 +0600

    url fix

--- src/main/java/com/probal/moddhobitto/api/user/controller/UserProfileController.java
+++ src/main/java/com/probal/moddhobitto/api/user/controller/UserProfileController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("api/user")
+@RequestMapping("/api/user")
 public class UserProfileController {
 
 
